00ed145f4a7060521b69bb7386efbb97a251fed4
Siddhartha587729 <90237294+Siddhartha587729@users.noreply.github.com>
2025-05-12 17:40:47 +0530

    hero and navbar components updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,40 @@
+import { ArrowDown, ArrowRight } from 'lucide-react';
 import bg from '../assets/example.jpg';
+import { CgArrowTopLeftR } from 'react-icons/cg';
 const Hero = () => {
     return (
-        <div style={{ backgroundImage: `url(${bg})` }} className=" relative h-[80vh] bg-cover bg-center flex flex-col items-center justify-center text-center rounded-3xl shadow-lg">
-            <div>
-                <h1 className="text-3xl font-bold text-white mb-4">
-                    Welcome to Your Website
-                </h1>
-                <p className="text-gray-100">
-                    This is your landing page content.
-                </p>
+        <div style={{ backgroundImage: `url(${bg})` }} className="relative h-[80vh] bg-cover bg-center flex flex-col items-center justify-center text-center rounded-3xl shadow-lg">
+            <div className='flex flex-col items-center justify-center gap-2'>
+                <span className='text-6xl text-white font-bold'>Where Quality </span>
+                <span className='text-6xl text-white font-bold'> Meets Functionality</span>
+                <span className='text-white text-wrap w-[60%]'>Delivering full-stack solutions that are fast, secure, and scalable.
+                    From sleek UIs to powerful backends — I bring ideas to life with clean code.</span>
             </div>
-            <div className="absolute top-90 left-200 w-80 h-90 rounded-xl drop-shadow-2xl">
-                <div className='flex w-full h-[35%] rounded-t-xl'>
-                    <div
-                        style={{
-                            clipPath: 'polygon(100% 0, 100% 100%, 0 100%)',
-                        }}
-                        className='h-full w-2/5 bg-white'>
-                        
-                    </div>
-                    <div className='h-full w-3/5 bg-white rounded-tr-xl'>
-                    </div>
-                </div>
-                <div className='w-full h-[65%] rounded-b-xl bg-white'>
+            <div className="absolute top-90 left-200 w-80 h-96 rounded-xl drop-shadow-2xl">
+                <div
+                    style={{
+                        clipPath: 'polygon( 0% 40%, 40% 0%, 40% 0%, 100% 0%, 100% 100%, 0% 100%)',
 
+                    }}
+                    className="w-80 h-96 bg-white rounded-2xl ">
+                    <div className='w-full h-[40%] p-4 rounded-t-2xl flex items-baseline justify-end'>
+                        <div className='flex items-center justify-center p-3 bg-secondary rounded-full hover:scale-105 transition-transform duration-300 hover:cursor-pointer'>
+                            <a href="/something"> <ArrowRight color='white' size={30} /></a>
+                        </div>
+                    </div>
+                    <div className='w-full h-[60%]  text-slate-600 rounded-b-2xl flex items-center justify-center'>
+                        <div className='flex flex-col items-center justify-center gap-2 h-full'>
+                            <div className='flex flex-col items-center justify-center gap-2 rounded-2xl p-4 h-[70%]'>
+                                <h1 className='text-2xl font-bold text-slate-900'>Get Started</h1>
+                                <p className='text-sm'>Learn, Generate, & Improve yourself with Kusray</p>
+                            </div>
+                            <div className='h-[30%] text-sm flex flex-col items-center justify-center gap-2'>
+                                <div className='h-1 w-[90%] border-t-1'></div>
+                                <p>3.5K+ Happy Users And Counting</p>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import logo from '../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="fixed top-0 left-0 w-full bg-white z-50 ">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 rounded-2xl shadow-[0_2px_6px_-3px_rgba(0,0,0,0.1)]">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <div className="flex-shrink-0 flex gap-2 items-center">
@@ -14,24 +14,24 @@ const Navbar = () => {
           
           {/* Navigation Links */}
           <div className="hidden md:block">
-            <div className="ml-10 flex items-center space-x-4">
-              <a href="/" className="text-gray-500 hover:text-gray-900 hover:border-gray-900 px-3 py-1 border-1 border-gray-400 rounded-4xl text-sm font-medium transition-colors duration-200">
+            <div className="ml-10 flex items-center space-x-4 font-semibold text-slate-700">
+              <a href="/" className=" hover:text-gray-900 hover:border-gray-900 px-3 py-1 border-1 border-slate-700 rounded-4xl text-sm  transition-colors duration-200">
                 Home
               </a>
-              <a href="/about" className="text-gray-500 hover:text-gray-900 hover:border-gray-900 px-3 py-1 border-1 border-gray-400 rounded-4xl text-sm font-medium transition-colors duration-200">
+              <a href="/about" className=" hover:text-gray-900 hover:border-gray-900 px-3 py-1 border-1 border-slate-700 rounded-4xl text-sm transition-colors duration-200">
                 About
               </a>
-              <a href="/services" className="text-gray-500 hover:text-gray-900 hover:border-gray-900 px-3 py-1 border-1 border-gray-400 rounded-4xl text-sm font-medium transition-colors duration-200">
+              <a href="/services" className=" hover:text-gray-900 hover:border-gray-900 px-3 py-1 border-1 border-slate-700 rounded-4xl text-sm transition-colors duration-200">
                 Team
               </a>
-              <a href="/contact" className="text-gray-500 hover:text-gray-900 hover:border-gray-900 px-3 py-1 border-1 border-gray-400 rounded-4xl text-sm font-medium transition-colors duration-200">
+              <a href="/contact" className=" hover:text-gray-900 hover:border-gray-900 px-3 py-1 border-1 border-slate-700 rounded-4xl text-sm transition-colors duration-200">
                 Services
               </a>
             </div>
           </div>
 
           <div>
-            <a href="/contact" className=" hidden md:flex items-center gap-1 border text-secondary hover:text-white hover:bg-secondary px-6 py-2 rounded-4xl text-sm font-medium transition-colors duration-200">
+            <a href="/contact" className=" hidden md:flex items-center gap-1 border text-secondary hover:text-white hover:bg-secondary px-6 py-2 rounded-4xl text-sm font-semibold transition-colors duration-200">
               Contact us <div className='bg-secondary rounded-full p-1'><MoveUpRight size={13} color='white'/></div>
             </a>
           </div>
